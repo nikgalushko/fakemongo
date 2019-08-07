6077754d853dea9f8b2249617817ae8fa42779bb
nikita <galnikrom@gmail.com>
2019-08-07 20:23:45 +0300

    update inner array

--- session/update_test.go
+++ session/update_test.go
@@ -13,6 +13,7 @@ var testDataUpd = []collection.Record{
 	{"f": 10, "arr": []interface{}{"t", "e", "s", "t"}, "arr2": []interface{}{5}},
 	{"e": 4},
 	{"f": 12, "obj": []interface{}{1, 2, 3}, "e": 5},
+	{"test": "#dot_notation", "obj": bson.M{"arr": []interface{}{12}}},
 }
 
 func TestUpdater_Update(t *testing.T) {
@@ -43,4 +44,14 @@ func TestUpdater_Update_Push_Each(t *testing.T) {
 	err := u.Update(query, update)
 	assert.NoError(t, err)
 	assert.Equal(t, collection.Record{"f": 12, "obj": []interface{}{1, 2, 3, 90, 91, 92}, "e": 5}, testDataUpd[4])
+}
+
+func TestUpdater_Update_Push_DotNotation(t *testing.T) {
+	query := bson.M{"test": "#dot_notation"}
+	update := bson.M{"$push": bson.M{"obj.arr": "2"}}
+	u := Updater{c: collection.NewCursor(&testDataUpd)}
+
+	err := u.Update(query, update)
+	assert.NoError(t, err)
+	assert.Equal(t, collection.Record{"test": "#dot_notation", "obj": bson.M{"arr": []interface{}{12, "2"}}}, testDataUpd[5])
 }
